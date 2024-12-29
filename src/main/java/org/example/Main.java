65771338b6d8b1693b473965480d71a715adf720
Sean Kembo <kembosean7@gmail.com>
2024-12-29 23:16:33 +0200

    refactoring code into other classes

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,51 +1,20 @@
 package org.example;
 import java.util.Arrays;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+
 public class Main {
-    public String findWinner(String[][] board) throws IllegalArgumentException{
-
-        if(board.length != 3 || board[0].length != 3 || board[1].length != 3 || board[2].length != 3){
-            throw new IllegalArgumentException("Board must be a 3x3 grid");
-        }
-
-        String[][] possibleWins = {
-                {board[0][0], board[0][1], board[0][2]},
-                {board[1][0], board[1][1], board[1][2]},
-                {board[2][0], board[2][1], board[2][2]},
-                {board[0][0], board[1][0], board[2][0]},
-                {board[0][1], board[1][1], board[2][1]},
-                {board[0][2], board[1][2], board[2][2]},
-                {board[0][0], board[1][1], board[2][2]},
-                {board[0][2], board[1][1], board[2][0]}
-        };
-
-        for (String[] line : possibleWins) {
-            if (line[0].equals("X") && line[1].equals("X") && line[2].equals("X")) {
-                return "X";
-            } else if (line[0].equals("O") && line[1].equals("O") && line[2].equals("O")) {
-                return "O";
-            } else if (!(line[0].equals("O") || line[0].equals("X")) || !(line[1].equals("O") || line[1].equals("X")) || !(line[2].equals("O") || line[2].equals("X"))) {
-                throw new IllegalArgumentException("Invalid character in board, Only 'X', 'O'");
-            }
-
-        }
-        return null;
-    }
     public static void main(String[] args){
 
-        Main ttt = new Main();
+        findWinner winner = new findWinner();
         String[][] board = {
-                {"O", "O"},
+                {"O", "X","X"},
                 {"O", "X", "O"},
-                {"X", "O", "X"}
+                {"O", "O", "X"}
         };
 
-        String winner = ttt.findWinner(board);
-
-        if (!(winner == null)){
-            System.out.println("Winner: "+ winner);
+        String gameWinner = winner.Winner(board);
+        if (!(gameWinner == null)){
+            System.out.println("Winner: "+ gameWinner);
         }
         else{
             System.out.println("No winner");
---
+++ src/main/java/org/example/findWinner.java
@@ -0,0 +1,33 @@
+package org.example;
+
+public class findWinner {
+    protected String Winner(String[][] board) throws IllegalArgumentException{
+
+        if(board.length != 3 || board[0].length != 3 || board[1].length != 3 || board[2].length != 3){
+            throw new IllegalArgumentException("Board must be a 3x3 grid");
+        }
+
+        String[][] possibleWins = {
+                {board[0][0], board[0][1], board[0][2]},
+                {board[1][0], board[1][1], board[1][2]},
+                {board[2][0], board[2][1], board[2][2]},
+                {board[0][0], board[1][0], board[2][0]},
+                {board[0][1], board[1][1], board[2][1]},
+                {board[0][2], board[1][2], board[2][2]},
+                {board[0][0], board[1][1], board[2][2]},
+                {board[0][2], board[1][1], board[2][0]}
+        };
+
+        for (String[] line : possibleWins) {
+            if (line[0].equals("X") && line[1].equals("X") && line[2].equals("X")) {
+                return "X";
+            } else if (line[0].equals("O") && line[1].equals("O") && line[2].equals("O")) {
+                return "O";
+            } else if (!(line[0].equals("O") || line[0].equals("X")) || !(line[1].equals("O") || line[1].equals("X")) || !(line[2].equals("O") || line[2].equals("X"))) {
+                throw new IllegalArgumentException("Invalid character in board, Only 'X', 'O'");
+            }
+
+        }
+        return null;
+    }
+}
